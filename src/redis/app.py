e3cf0b5eac44536374ffb11a8e2181cb77314778
Doug <asd@asd.com>
2019-08-28 19:06:15 -0300

    adds read_by_id method

--- src/redis/app.py
+++ src/redis/app.py
@@ -19,5 +19,13 @@ class StartRedis(object):
         """ Reads data"""
         return self.redis.xread({self.stream:b"0-0"})
 
+    def read_by_id(self, id):
+        """ Reads by id"""
+        return self.read_stream()[0][1][id]
+
+    def len_of_stream(self):
+        """ Length of stream """
+        return self.redis.xlen(self.stream)
+
 
 data = {"hello": StartRedis('172.20.0.4').encoder.encode([1234, 125, 1235, 1235])}  # converts list to string
