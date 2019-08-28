aa9ae920d6d15d4d7f0cb79a1ca1aabc5528ec77
Doug <asd@asd.com>
2019-08-28 17:03:46 -0300

    starts redis implementation

--- src/redis/app.py
+++ src/redis/app.py
@@ -1,4 +1,5 @@
 import redis
+import json
 
 
 class StartRedis(object):
@@ -7,3 +8,16 @@ class StartRedis(object):
 
     def __init__(self, localhost):
         self.redis = redis.Redis(localhost)
+        self.encoder = json.JSONEncoder()
+        self.stream = 'doug_redis_stream'
+
+    def add_to_stream(self, data):
+        """ Adds data"""
+        return self.redis.xadd(self.stream, data)
+
+    def read_stream(self):
+        """ Reads data"""
+        return self.redis.xread({self.stream:b"0-0"})
+
+
+data = {"hello": StartRedis('172.20.0.4').encoder.encode([1234, 125, 1235, 1235])}  # converts list to string
